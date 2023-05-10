050cf43ca712313169d82174635dabfed90f5c76
Your Name <you@example.com>
2023-05-09 21:48:56 -0700

    basic cleanup

--- src/Views/page1/index.tsx
+++ src/Views/page1/index.tsx
@@ -41,20 +41,6 @@ const Page1 = () => {
         justifyContent={"center"}
         style={{ paddingTop: 20 }}
       >
-        {/* <Grid item>
-          <DirectionsCarFilledIcon color="secondary" sx={{ fontSize: 100 }} />
-        </Grid>
-
-        <Grid item style={{ margin: 2 }}>
-          <Button
-            size="large"
-            style={{ width: 280 }}
-            variant="contained"
-            onClick={() => Navigate()}
-          >
-            Start Quote
-          </Button>
-        </Grid> */}
         <Grid
           item
           width={600}
@@ -63,62 +49,7 @@ const Page1 = () => {
         >
           <Grid container>
             <Grid item height={60} width={"100%"}>
-              <Grid container direction={"row"}>
-                <Grid
-                  onClick={() => SetSelect(1)}
-                  item
-                  xs={6}
-                  height={60}
-                  style={{
-                    textAlign: "center",
-                    borderTop:
-                      select == 1 ? "2px solid #42b6f5" : "2px solid #d1d1d1",
-                    borderRight: select == 1 ? "2px solid #42b6f5" : "",
-                    borderLeft:
-                      select == 1 ? "2px solid #42b6f5" : "2px solid #d1d1d1",
-                    borderBottom: select == 2 ? "2px solid #42b6f5" : "",
-                    cursor: "pointer",
-                  }}
-                >
-                  <Typography
-                    style={{
-                      fontSize: 22,
-                      fontWeight: 600,
-                      color: "#42b6f5",
-                      marginTop: 10,
-                    }}
-                  >
-                    Personal
-                  </Typography>
-                </Grid>
-                <Grid
-                  item
-                  xs={6}
-                  height={60}
-                  onClick={() => SetSelect(2)}
-                  style={{
-                    textAlign: "center",
-                    borderTop:
-                      select == 2 ? "2px solid #42b6f5" : "2px solid #d1d1d1",
-                    borderRight:
-                      select == 2 ? "2px solid #42b6f5" : "2px solid #d1d1d1",
-                    borderLeft: select == 2 ? "2px solid #42b6f5" : "",
-                    borderBottom: select == 1 ? "2px solid #42b6f5" : "",
-                    cursor: "pointer",
-                  }}
-                >
-                  <Typography
-                    style={{
-                      fontSize: 22,
-                      fontWeight: 640,
-                      color: "#42b6f5",
-                      marginTop: 10,
-                    }}
-                  >
-                    Business
-                  </Typography>
-                </Grid>
-              </Grid>
+            
             </Grid>
             <Grid
               item
@@ -132,7 +63,7 @@ const Page1 = () => {
               }}
             >
               <Typography style={{ fontWeight: "bold", fontSize: 30 }}>
-                Get A Quote
+                Let's get started.
               </Typography>
               {select == 1 ? (
                 <Grid
@@ -142,64 +73,6 @@ const Page1 = () => {
                   width={"100%"}
                   justifyContent="space-between"
                 >
-                  <Grid
-                    onClick={() => Navigate()}
-                    item
-                    xs={3.5}
-                    height={160}
-                    style={{
-                      border: "2px solid #42b6f5",
-                      borderRadius: 10,
-                      cursor: "pointer",
-                    }}
-                  >
-                    <Grid>
-                      <DirectionsCarFilledIcon
-                        sx={{ fontSize: 100, color: "#42b6f5" }}
-                      />
-                    </Grid>
-                    <Typography style={{ fontSize: 22, fontWeight: "bold" }}>
-                      Car
-                    </Typography>
-                  </Grid>
-                  <Grid
-                    item
-                    xs={3.5}
-                    height={160}
-                    style={{
-                      border: "2px solid #42b6f5",
-                      borderRadius: 10,
-                      cursor: "pointer",
-                    }}
-                  >
-                    <Grid>
-                      <DirectionsCarFilledIcon
-                        sx={{ fontSize: 100, color: "#42b6f5" }}
-                      />
-                    </Grid>
-                    <Typography style={{ fontSize: 22, fontWeight: "bold" }}>
-                      Car
-                    </Typography>
-                  </Grid>
-                  <Grid
-                    item
-                    xs={3.5}
-                    height={160}
-                    style={{
-                      border: "2px solid #42b6f5",
-                      borderRadius: 10,
-                      cursor: "pointer",
-                    }}
-                  >
-                    <Grid>
-                      <DirectionsCarFilledIcon
-                        sx={{ fontSize: 100, color: "#42b6f5" }}
-                      />
-                    </Grid>
-                    <Typography style={{ fontSize: 22, fontWeight: "bold" }}>
-                      Car
-                    </Typography>
-                  </Grid>
                 </Grid>
               ) : (
                 <Grid
@@ -209,44 +82,6 @@ const Page1 = () => {
                   width={"100%"}
                   justifyContent="space-between"
                 >
-                  <Grid
-                    item
-                    xs={3.5}
-                    height={160}
-                    style={{
-                      border: "2px solid #42b6f5",
-                      borderRadius: 10,
-                      cursor: "pointer",
-                    }}
-                  >
-                    <Grid>
-                      <EmojiTransportationIcon
-                        sx={{ fontSize: 100, color: "#42b6f5" }}
-                      />
-                    </Grid>
-                    <Typography style={{ fontSize: 22, fontWeight: "bold" }}>
-                      Car
-                    </Typography>
-                  </Grid>
-                  <Grid
-                    item
-                    xs={3.5}
-                    height={160}
-                    style={{
-                      border: "2px solid #42b6f5",
-                      borderRadius: 10,
-                      cursor: "pointer",
-                    }}
-                  >
-                    <Grid>
-                      <EmojiTransportationIcon
-                        sx={{ fontSize: 100, color: "#42b6f5" }}
-                      />
-                    </Grid>
-                    <Typography style={{ fontSize: 22, fontWeight: "bold" }}>
-                      Car
-                    </Typography>
-                  </Grid>
                   <Grid
                     item
                     xs={3.5}
